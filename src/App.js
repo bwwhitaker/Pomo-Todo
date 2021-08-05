3d2c2459f1f9df83028191eece4932b61e2f1365
Brian Whitaker <43477669+bwwhitaker@users.noreply.github.com>
2021-08-04 21:42:27 -0700

    Added SignIn and SignOut Buttons

--- src/App.js
+++ src/App.js
@@ -8,13 +8,23 @@ import Header from './Components/Header';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
 
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
 function App() {
 	return (
-		<Router>
-			<Header />
-			<Route exact path='/' component={Home} />
-			<Footer />
-		</Router>
+		<div>
+			<AmplifyAuthenticator>
+				<Router>
+					<Header />
+					<Route exact path='/' component={Home} />
+					<Footer />
+				</Router>
+			</AmplifyAuthenticator>
+		</div>
 	);
 }
 
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,12 +2,33 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import '../App.css';
+import Button from 'react-bootstrap/Button';
+
+import { Auth } from 'aws-amplify';
+
+async function signOut() {
+	try {
+		await Auth.signOut();
+	} catch (error) {
+		console.log('error signing out: ', error);
+	}
+}
 
 function Footer() {
 	return (
-		<Navbar bg='dark' variant='dark' className='justify-content-end' fixed='bottom' sticky='bottom'>
-			<Nav className='footer'>Pomo-Todo v0.1</Nav>
-		</Navbar>
+		<div>
+			<Navbar bg='dark' variant='dark' fixed='bottom' sticky='bottom' className='footer'>
+				<Nav>
+					<Button variant='secondary' size='sm' onClick={() => signOut()}>
+						Sign Out
+					</Button>
+				</Nav>
+
+				<Navbar.Collapse className='justify-content-end'>
+					<Nav>Pomo-Todo v0.1</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</div>
 	);
 }
 
