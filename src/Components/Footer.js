f73b402d941211b793d03b8c716ffbc7be8d253a
Brian Whitaker <43477669+bwwhitaker@users.noreply.github.com>
2022-03-01 20:58:04 -0800

    RemoveAuth

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,27 +2,12 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import '../App.css';
-import Button from 'react-bootstrap/Button';
-
-import { Auth } from 'aws-amplify';
-
-async function signOut() {
-	try {
-		await Auth.signOut();
-	} catch (error) {
-		console.log('error signing out: ', error);
-	}
-}
 
 function Footer() {
 	return (
 		<div>
 			<Navbar bg='dark' variant='dark' fixed='bottom' sticky='bottom' className='footer'>
-				<Nav>
-					<Button variant='secondary' size='sm' onClick={() => signOut()}>
-						Sign Out
-					</Button>
-				</Nav>
+				<Nav></Nav>
 
 				<Navbar.Collapse className='justify-content-end'>
 					<Nav>Pomo-Todo v0.1</Nav>
